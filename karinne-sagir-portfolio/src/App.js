217e5ad12de7354bfffa6749dc8a91808aa7523c
Karinne <karinne.sagir@gmail.com>
2023-04-02 13:38:21 -0700

    added components and updated app.js and index.js

--- karinne-sagir-portfolio/src/App.js
+++ karinne-sagir-portfolio/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// We import our Welcome component from our components folder so that we can eventually return it.
+import Welcome from './components/Welcome';
 
-function App() {
+// Inside our App component, we have a return method that contains all the JSX we want to render to the screen.
+// In this example, we have a parent `div` that references the Welcome component that we imported at the top.
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Welcome />
     </div>
   );
 }
 
-export default App;
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
