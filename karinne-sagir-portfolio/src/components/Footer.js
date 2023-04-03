59f81df71ad708b6ccd766c788b3083dc8669fec
Karinne <karinne.sagir@gmail.com>
2023-04-02 23:12:41 -0700

    added header and footer

---
+++ karinne-sagir-portfolio/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <div class="footer" id="footer">
+        <a href="https://github.com/karinnesagir"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" class="icon"/></a>
+        <a href="https://github.com/karinnesagir"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" class="icon"/></a>
+        <a href="https://stackoverflow.com/users/21553869/karinne"><img src="https://img.icons8.com/color/48/000000/stackoverflow.png" alt="Stack Overflow" class="icon"/></a>
+    </div>
+    )
+}
+
+export default Footer;
---
+++ karinne-sagir-portfolio/src/components/Header.js
@@ -0,0 +1,17 @@
+import React  from 'react';
+
+function Header() {
+    return (
+        <div className="container">
+        <h1>Karinne Sagir</h1>
+        <ul className="parent">
+            <li>About Me</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+            <li>Resume</li>
+        </ul>
+        </div>
+    )
+}
+
+export default Header;
